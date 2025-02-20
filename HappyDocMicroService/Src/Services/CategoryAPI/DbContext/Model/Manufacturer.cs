bb7b94a1388ab5159e97ca4c2ba4047736a11fea
rakibhossaingalib <rakib@primetechbd.com>
2025-02-20 18:16:07 +0600

    DbContextOngoing

---
+++ HappyDocMicroService/Src/Services/CategoryAPI/DbContext/Model/Manufacturer.cs
@@ -0,0 +1,13 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace CategoryAPI.DbContext.Model
+{
+    public class Manufacturer
+    {
+        [BsonId]
+
+        public int ManufacturerId { get; set; }
+
+        public string ManufacturerName { get; set; }
+    }
+}
