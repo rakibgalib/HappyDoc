f6df85cdd09031a1e6fc142742233c198bee13ba
rakibhossaingalib <rakib@primetechbd.com>
2025-02-20 22:33:59 +0600

    db context updated

--- HappyDocMicroService/Src/Services/CategoryAPI/DbContext/CategoryDbContext.cs
+++ HappyDocMicroService/Src/Services/CategoryAPI/DbContext/CategoryDbContext.cs
@@ -4,6 +4,7 @@ namespace CategoryAPI.DbContext
 {
     public class CategoryDbContext:ApplicationDbContext
     {
+        static string ConnectionString=new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json",true,true).Build().GetConnectionString("Category.API")
         public CategoryDbContext(string connectionString,string databaseName) : base(connectionString,databaseName){
 
         }
